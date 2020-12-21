4624f6af3848c8b874af9cc7499b4c448bd4390a
sushm <sushm@DESKTOP-TAMPQGL>
2020-12-21 16:27:54 +0530

    test

---
+++ src/test/java/steps/test.java
@@ -0,0 +1,7 @@
+package steps;
+import cucumber.api.java8.StepdefBody.A0;
+
+public class test implements A0 {
+	
+
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,6 @@
+import cucumber.api.java8.StepdefBody.A0;
+
+public class test implements A0 {
+	
+
+}
